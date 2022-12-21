16278eaa7286036d63fb3b3b457df7639939f3df
Diana <dianadauksa@gmail.com>
2022-12-21 15:28:35 +0200

    Update UsersRepository.php

--- app/Repositories/Users/UsersRepository.php
+++ app/Repositories/Users/UsersRepository.php
@@ -9,10 +9,7 @@ interface UsersRepository
     public function add(RegisterUserServiceRequest $request): void;
     public function getByEmail(string $email): ?array;
     public function getByID(int $id): ?array;
-    public function getUserStocks(int $id): ?array;
     public function getAmountOwned(int $id, string $symbol): ?int;
     public function getUserStock(int $id, string $symbol): ?array;
-    public function getTransactionsByStock(int $id, string $symbol): ?array;
-    public function getAllTransactions(int $id): ?array;
     public function getAllUsers(): ?array;
 }
